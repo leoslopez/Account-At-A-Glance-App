2968ae255d1dc5e4935a541ae49ca14c1403b47f
Leo Lopez <llopez@makingsense.com>
2012-11-27 11:30:33 -0300

    Bind data in the templates using characteristics of the jQuery Templating Plugin

--- AccountAtAGlance/NewsAtAGlance/Scripts/ScenesHandlers/scene.tile.formatter.js
+++ AccountAtAGlance/NewsAtAGlance/Scripts/ScenesHandlers/scene.tile.formatter.js
@@ -6,16 +6,20 @@ var tileFormatter = new function () {
 
     tmplSizes = ['Small', 'Medium', 'Large'],
 
+    
     formatNews = function (tileDiv) {
-        var lblNewsTitle = $('#lblNewsTitle', tileDiv);
-        var lnkNews = $('#lnkNews', tileDiv);
-        var lblNewsSnippet = $('#lblNewsSnippet', tileDiv);
-        var spNewsSource = $('#spNewsSource', tileDiv);
+        // This code could be used to set information in the news template.
+        // But I am using jquery template plugin so it is not necessary.
 
-        lblNewsTitle.text(tileDiv.data().tileData.Title);
-        lnkNews.attr('href', tileDiv.data().tileData.Url);        
-        lblNewsSnippet.text(tileDiv.data().tileData.Snippet);
-        spNewsSource.text(tileDiv.data().tileData.Source);
+//        var lblNewsTitle = $('#lblNewsTitle', tileDiv);
+//        var lnkNews = $('#lnkNews', tileDiv);
+//        var lblNewsSnippet = $('#lblNewsSnippet', tileDiv);
+//        var spNewsSource = $('#spNewsSource', tileDiv);
+
+//        lblNewsTitle.text(tileDiv.data().tileData.Title);
+//        lnkNews.attr('href', tileDiv.data().tileData.Url);        
+//        lblNewsSnippet.text(tileDiv.data().tileData.Snippet);
+//        spNewsSource.text(tileDiv.data().tileData.Source);
 
     };
 
