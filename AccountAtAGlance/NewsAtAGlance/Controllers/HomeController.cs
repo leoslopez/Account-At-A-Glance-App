6a19fe4c65bc31cc920e33bb740cd18d3bd4ff15
Leo Lopez <llopez@makingsense.com>
2012-11-30 09:21:18 -0300

    Remove old code used to display the raw news

--- AccountAtAGlance/NewsAtAGlance/Controllers/HomeController.cs
+++ AccountAtAGlance/NewsAtAGlance/Controllers/HomeController.cs
@@ -14,19 +14,7 @@ namespace NewsAtAGlance.Controllers
 
         public ActionResult Index()
         {
-            List<News> news = new List<News>();
-
-           // TODO: "0" identify "Top Stories", replace this hard coded by enum.
-            try
-            {
-                news = _newsRepository.GetNews("es", "0", false);
-            }
-            catch (Exception ex)
-            {
-                new LogEvent(ex.Message);
-            }
-
-            return View(news);
+           return View();
         }        
 
     }
